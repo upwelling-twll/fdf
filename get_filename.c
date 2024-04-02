746431a617a910449e9465cb2c1b4f128fa8a00f
Natalia Magdanova <nmagdano@lab2r4s10.42abudhabi.ae>
2024-04-02 23:06:24 +0400

    correct_file function added to handle extension

--- get_filename.c
+++ get_filename.c
@@ -6,11 +6,25 @@
 /*   By: nmagdano <nmagdano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 13:40:27 by nmagdano          #+#    #+#             */
-/*   Updated: 2024/04/01 23:30:59 by nmagdano         ###   ########.fr       */
+/*   Updated: 2024/04/02 23:05:36 by nmagdano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+static int	correct(char *file)
+{
+	int	len;
+
+	len = ft_strlen(file);
+	if (!len || len <= 4)
+		return (1);
+	len--;
+	if (file[len] == 'f' && file[len - 1] == 'd' && file[len - 2] == 'f'
+		&& file[len - 3] == '.')
+		return (0);
+	return (1);
+}
 
 char	*get_file(char *argv)
 {
@@ -21,6 +35,8 @@ char	*get_file(char *argv)
 	if (argv)
 	{
 		file = argv;
+		if (correct(file))
+			return (NULL);
 		n = open(file, O_RDONLY);
 		if (n > 2)
 		{
